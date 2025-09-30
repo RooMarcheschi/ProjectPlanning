d4b0df651fc3faabb8a509d17838d322f53630ce
Maria Rosa Marcheschi <romarcheschi@gmail.com>
2025-09-29 23:20:12 -0300

    quick fix

--- backend/routers/proyectos.py
+++ backend/routers/proyectos.py
@@ -1,23 +1,20 @@
 from fastapi import APIRouter, Body, Depends
 from sqlalchemy.orm import Session
-
-from config.database import get_db
 from services import proyecto_service, etapa_service
 from schemas.proyectos import ProyectoCreate, ProyectoOut
 from models.proyecto import Proyecto, EstadoProyecto
 from models.etapa import Etapa, EstadoEtapa
 from datetime import date
-from config.database import get_db as db
+from config.database import get_db
 
 router = APIRouter(
     prefix="/proyectos",
     tags=["Proyectos"]
 )
 
-
 # Crear un proyecto
-@router.post("/crearProyecto", response_model=ProyectoOut)
-def crear_proyecto(proyecto: dict = Body(...)):
+@router.post("/crearProyecto")
+def crear_proyecto(proyecto: dict = Body(...), db: Session = Depends(get_db)):
     ong_Name = proyecto["ongName"]
     project_name = proyecto["projectName"]
     amount_stages = proyecto["stagesAmount"]
@@ -25,7 +22,7 @@ def crear_proyecto(proyecto: dict = Body(...)):
     if not ong_Name or type(ong_Name) != str or ong_Name.strip() == "":
         return {"success": False, "message": "Invalid ONG name"}
 
-    if not project_name or type(project_name) != str or ong_Name.strip() == "":
+    if not project_name or type(project_name) != str or project_name.strip() == "":
         return {"success": False, "message": "Invalid Project name"}
 
     if not amount_stages or type(amount_stages) != int:
@@ -37,19 +34,20 @@ def crear_proyecto(proyecto: dict = Body(...)):
         if not name or type(name) != str or name.strip() == "" or not desc or type(desc) != str or desc.strip() == "":
             return {"success": False, "message": f"Error with stage {i+1}"}
         
+    print("probando try")
     try:
-        proyecto = Proyecto(titulo=project_name, descripcion="", fecha_creacion=date.today(), estado=EstadoProyecto.publicado) 
-        proyecto = proyecto_service.crear_proyecto(db, proyecto)
+        proy = Proyecto(titulo=project_name, descripcion="", fecha_creacion=date.today(), estado=EstadoProyecto.publicado) 
+        nuevo_proyecto = proyecto_service.crear_proyecto(db, proy)
 
         for i, stage in enumerate(proyecto["stages"]):
             name = stage["name"]
             desc = stage["description"]
             # tenemos que chequear fecha inicio y fecha fin
-            etapa = Etapa(id_proyecto=proyecto.id, titulo=name, descripcion=desc, fecha_creacion=date.today(), fecha_inicio=date.today(), fecha_fin=date.today(), estado=EstadoEtapa.publicada)    
-            etapa = etapa_service.crear_etapa(db, etapa)
+            etapa = Etapa(id_proyecto=nuevo_proyecto.id, titulo=name, descripcion=desc, fecha_creacion=date.today(), fecha_inicio=date.today(), fecha_fin=date.today(), estado=EstadoEtapa.publicada)    
+            nueva_etapa = etapa_service.crear_etapa(db, etapa)
         return {"success": True, "message": "Project submitted successfully"}
     
-    except:
-        return {"success": False, "message": f"Error with db"}
+    except Exception as e:
+        return {"success": False, "message": f"Error with db :{e}"}
 
 
