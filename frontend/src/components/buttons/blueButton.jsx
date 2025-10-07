4b5d89e31e351966b783b8fb980ffc3b54577bf2
Juan Cruz de Urquiza <deurquizajuancruz@gmail.com>
2025-10-07 20:32:49 -0300

    landing page and more

--- frontend/src/components/buttons/blueButton.jsx
+++ frontend/src/components/buttons/blueButton.jsx
@@ -1,6 +1,6 @@
 function BlueButton({ text, type = null, classAttr = null }) {
     return (
-        <button className={`bg-blue-600 text-white px-4 py-2 rounded hover:cursor-pointer hover:scale-105 transition font-semibold${classAttr ? ' ' + classAttr : ''}`} type={type}>
+        <button className={`bg-blue-600 text-white px-4 py-2 rounded hover:cursor-pointer hover:scale-102 transition font-semibold${classAttr ? ' ' + classAttr : ''}`} type={type}>
             {text}
         </button>
     )
---
+++ frontend/src/components/projects/rectangle.jsx
@@ -0,0 +1,22 @@
+function Rectangle({ title }) {
+    return (
+        <a href="/cargarProyecto" className="flex flex-col justify-center items-center bg-gray-200 rounded-2xl shadow-md hover:shadow-xl transition-shadow cursor-pointer">
+            {title === "Crear proyecto" ? (
+                <span className="text-6xl text-black">+</span>
+            ) : (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-16 w-16 text-blue-600"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 7h18M3 12h18m-7 5h7" />
+                </svg>
+            )}
+            <h3 className="text-lg font-semibold mt-2">{title}</h3>
+        </a>
+    );
+}
+
+export default Rectangle;
