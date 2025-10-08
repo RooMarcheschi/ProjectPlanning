ac531830b7164be8b9f64206ae9857d2cf4d54ef
Maria Rosa Marcheschi <romarcheschi@gmail.com>
2025-10-07 22:56:15 -0300

    creo servicios de user y ong

---
+++ backend/services/user_service.py
@@ -0,0 +1,28 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Session
+from models.user import User
+
+def crear_usuario(db: Session, nuevo_usuario: User):
+    db.add(nuevo_usuario)
+    db.commit()
+    db.refresh(nuevo_usuario)
+    return nuevo_usuario
+
+def obtener_usuario(db: Session):
+    return db.query(User).all()
+
+def obtener_usuario_por_id(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def obtener_usuario_por_email(db: Session, user_email: String):
+    return db.query(User).filter(User.email == user_email).first()
+
+def obtener_usuario_por_username(db: Session, user_username: String):
+    return db.query(User).filter(User.username == user_username).first()
+
+def eliminar_usuario(db: Session, user_id: int):
+    usuario = db.query(User).filter(User.id == user_id).first()
+    if usuario:
+        db.delete(usuario)
+        db.commit()
+    return usuario
