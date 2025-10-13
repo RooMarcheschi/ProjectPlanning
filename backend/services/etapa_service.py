4fe47c5892380028499498b6989a16372337659d
Maria Rosa Marcheschi <romarcheschi@gmail.com>
2025-10-12 22:52:20 -0300

    service de etapa que devuelve la cant de etapas cubiertas para un proyecto

--- backend/services/etapa_service.py
+++ backend/services/etapa_service.py
@@ -37,3 +37,10 @@ def get_etapa_by_id(id: int, db: Session):
     if etapa:
         etapa.ong = db.query(Proyecto).filter(Proyecto.id == etapa.id_proyecto).first().ong
     return etapa
+
+def cant_etapas_cubiertas_por_proyecto(db: Session, proyecto_id: int):
+    return (db.query(Etapa).filter(
+        Etapa.id_proyecto==proyecto_id,
+        Etapa.estado==EstadoEtapa.cubierta
+    ).scalar()
+    or 0)
