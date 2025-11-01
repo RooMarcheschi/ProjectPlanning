62b2727ca59960b9771187bc1ab3c9c473e180f8
Ladislao Bordon <ladislao.bordon@gmail.com>
2025-11-01 19:16:32 -0300

    inicio para subir compromiso

--- backend/routers/compromisos.py
+++ backend/routers/compromisos.py
@@ -1,11 +1,15 @@
 from datetime import date
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
+from fastapi.security import OAuth2PasswordBearer
+from core.security import decode_token
 from config.database import get_db
 from services import compromiso_service
 from models.compromiso import Compromiso, EstadoCompromiso
 from models.etapa import Etapa
+from dependencies import get_bonita_client
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 router = APIRouter(
     prefix="/compromisos",
@@ -17,7 +21,14 @@ def get_all_compromisos(name: str, db: Session = Depends(get_db)):
     return compromiso_service.obtener_compromisos(db=db)
 
 @router.post("/asumir")
-def asumir_compromiso(etapa_id: int, contribuyente_id: int, db: Session = Depends(get_db)):
-    compromiso = compromiso_service.asumir_compromiso(db=db, etapa_id=etapa_id, contribuyente_id=contribuyente_id)
-
-    return {"message": "Compromiso generado correctamente", "compromiso_id": compromiso.id}
+def asumir_compromiso(etapa_id: int, db: Session = Depends(get_db),token: str = Depends(oauth2_scheme)):
+    username = decode_token(token)
+    #compromiso = compromiso_service.asumir_compromiso(db=db, etapa_id=etapa_id, contribuyente_id=contribuyente_id)
+    bonita = get_bonita_client()
+    #Conseguir id del case
+    #Setear JWT del user en bonia
+    #Setear el compromiso con el etapa_id
+    
+    #Avanzar las actividades en Bonita 3 veces
+    
+    return {"message": "Compromiso generado correctamente"}
