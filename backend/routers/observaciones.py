d64f55610a92a22675da6bb8ddc467007a328a24
Ladislao Bordon <ladislao.bordon@gmail.com>
2025-11-16 20:17:28 -0300

    generar case observacion,cancelar obserbacion, realizar observacion conectado con bonita

--- backend/routers/observaciones.py
+++ backend/routers/observaciones.py
@@ -1,3 +1,4 @@
+from dependencies import debug, get_bonita_client, wait_for_any_activity, wait_for_ready_activity
 from config.database import get_db
 from core.security import decode_token
 from datetime import date
@@ -17,7 +18,22 @@ class ObservacionCreate(BaseModel):
     observante_id: int
     descripcion: str
 
+@router.post("/generar_case")
+def generar_case(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    username = decode_token(token)
+    if not username:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    try:
+        bonita = get_bonita_client()
+        process_id = bonita.get_process_id_by_name("CargarObservacion")
 
+
+        result = bonita.start_process(process_definition_id=process_id)
+        
+        #Cargar 
+        return {"success": True, "message": "Case de observacion generado correctamente", "case_id": result["caseId"]}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error generating case in Bonita: " + str(e))
 @router.get("/")
 def get_all_observaciones(
     db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)
@@ -25,10 +41,29 @@ def get_all_observaciones(
     username = decode_token(token)
     return observacion_service.obtener_observaciones(db=db)
 
-
+@router.post("/cancelar_observacion")
+def cancelar_observacion(
+    case_id: int,
+    db: Session = Depends(get_db),
+    token: str = Depends(oauth2_scheme),
+):
+    username = decode_token(token)
+    user = user_service.obtener_usuario_por_username(db=db, user_username=username)
+    if not user:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+    try:
+        bonita = get_bonita_client()
+        activities = wait_for_any_activity(bonita, case_id)
+        task1 = activities[0]["id"]
+        bonita.assign_task(task_id=task1, user_id=1)
+        bonita.complete_activity(task_id=task1)
+        return {"success": True, "message": "Observacion cancelada correctamente"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail={"message": str(e)})
 @router.post("/realizar_observacion")
 def realizar_observacion(
     obs: ObservacionCreate,
+    case_id: int,
     db: Session = Depends(get_db),
     token: str = Depends(oauth2_scheme),
 ):
@@ -49,13 +84,34 @@ def realizar_observacion(
             db=db, nueva_observacion=obs
         )
         # Acá es donde debería avanzar el proceso en Bonita
+        try:
+            bonita = get_bonita_client()
+            bonita.set_case_variable(
+            case_id=case_id,
+            variable_name="hayObservaciones",
+            value=True,
+            type_hint="java.lang.Boolean",
+            )
+            activities = wait_for_any_activity(bonita, case_id)
+            task1 = activities[0]["id"]
+
+            bonita.assign_task(task_id=task1, user_id=1)
+            bonita.complete_activity(task_id=task1)
+            last_id = task1
+            for i in range(2):
+                act = wait_for_ready_activity(bonita, case_id, previous_id=last_id)
+                bonita.assign_task(task_id=act["id"], user_id=2)
+                bonita.complete_activity(task_id=act["id"])
+                last_id = act["id"]
+        except Exception as e:
+            debug("No se pudo avanzar el proceso en Bonita:", str(e))
+            
         return {
             "success": True,
             "observacion": observacion,
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail={"message": str(e)})
-
 
 # @router.post("/eliminar_observacion")
 # def eliminar_observacion(
